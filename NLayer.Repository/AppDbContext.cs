8feef0e41983612f57f8684e89e72cb040a5a648
celil öztürk <celil.ozturk@windowslive.com>
2022-11-06 01:41:20 +0100

    Add project files.

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using NLayer.Repository.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            //Tek tek yapmak yerine Assembly'deki tum configurationlari bul ve uygula diyebiliyoruz !!!!
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<ProductFeature>().HasData(
+                new ProductFeature() { Id=1,Color="Kirmizi",Height=100,Width=200,ProductId=1},
+                new ProductFeature() { Id = 2, Color = "Mavi", Height = 300, Width = 500, ProductId = 2 }
+                );
+            
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
