79c18fee5b174273e554bc6708bca0207e7a9f7c
celil öztürk <celil.ozturk@windowslive.com>
2022-11-06 20:52:53 +0100

    Caching section implemented.

--- NLayer.API/Modules/RepoServiceModule.cs
+++ NLayer.API/Modules/RepoServiceModule.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using NLayer.Caching;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
@@ -29,6 +30,7 @@ namespace NLayer.API.Modules
 
             builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
             builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterType<ProductServiceWithCaching>().As<IProductService>();
 
             //InstancePerLifetimeScope=> Scope (Service ekleme metodu olarak buna karsilik gelir.)
             //InstancePerDependency  =>Transient
